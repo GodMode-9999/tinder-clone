b0d9aacdb03e09c9f69c97029343027e4b88146b
Vikas Rathod <115883749+GodMode-9999@users.noreply.github.com>
2023-10-10 10:24:35 +0530

    first backend files are added

---
+++ server.js
@@ -0,0 +1,52 @@
+import express  from "express";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js"
+import cors from 'cors'
+
+// App Config 
+const app = express() ;
+const port = process.env.PORT || 8001 ;
+const db_url ="mongodb+srv://rathodvikas250:ejgFVusQqP3D5xbG@cluster0.ctwhbi9.mongodb.net/?retryWrites=true&w=majority"
+
+
+// MidleWares
+app.use(express.json()) ;
+app.use(cors()) ;
+
+// DB config
+mongoose.connect(db_url , {
+    useNewUrlParser : true ,
+    // useCreatIndex : true ,
+    useUnifiedTopology : true ,
+})
+
+
+// Api Endpoints
+app.get('/' , (req,res) => res.status(200).send("hello vikas rathod...")) ;
+
+app.post("/tinder/cards" , (req , res) => {
+    const dbCard = req.body ;
+
+    Cards.create(dbCard , (err , data) => {
+        if(err) {
+            res.status(500).send(err)
+        }else {
+            res.status(201).send(data)
+        }
+    })
+
+})
+app.get("/tinder/cards" , (req , res) => {
+
+    Cards.find((err , data) => {
+        if(err) {
+            res.status(500).send(err)
+        }else {
+            res.status(200).send(data)
+        }
+    })
+
+})
+
+// Listener
+app.listen(port , () => console.log(`listening on localhost: ${port}`)) ;
